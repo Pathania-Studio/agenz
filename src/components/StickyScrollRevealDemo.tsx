1e9e42eef7ee60edcf1c124de2526e42f9d55cc4
NikhilPathania1 <nikhil.pathania@pionglobal.com>
2026-01-29 22:04:42 +0530

    new mockup

--- src/components/StickyScrollRevealDemo.tsx
+++ src/components/StickyScrollRevealDemo.tsx
@@ -19,12 +19,12 @@ const content = [
     title: "Digital Marketing",
     description: "Full productivity on desktop and laptop screens.",
     content: (
-      <div className="relative w-[620px] h-[420px] mx-auto">
+      <div className="relative w-[790px] h-[520px] mx-auto">
         {/* Laptop Frame */}
         <img src="images/laptop-frame.png" alt="Laptop Mockup" className="w-full h-full object-contain pointer-events-none select-none" />
 
         {/* Screen Content */}
-        <div className="absolute left-[12%] top-[8%] w-[76%] h-[74%] overflow-hidden rounded-md bg-black shadow-inner">
+        <div className="absolute left-[12%] top-[13%] w-[76%] h-[74%] overflow-hidden rounded-md bg-black shadow-inner">
           <img src="https://images.unsplash.com/photo-1562577309-4932fdd64cd1" alt="App UI" className="w-full h-full object-cover" />
         </div>
       </div>
